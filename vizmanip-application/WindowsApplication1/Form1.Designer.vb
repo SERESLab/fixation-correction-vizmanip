512bdadb320f60a5362a14900800a5acfe998e9f
Chris Palmer <capalmer1013@gmail.com>
2015-07-08 14:29:48 -0400

    initial commit

---
+++ vizmanip-application/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,234 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.OpenFileDialog2 = New System.Windows.Forms.OpenFileDialog()
+        Me.CheckBox4 = New System.Windows.Forms.CheckBox()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(181, 2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Load Image"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 32)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(868, 497)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(262, 3)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Load Data"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.AutoScroll = True
+        Me.Panel1.Controls.Add(Me.Button4)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.NumericUpDown2)
+        Me.Panel1.Controls.Add(Me.NumericUpDown1)
+        Me.Panel1.Controls.Add(Me.CheckBox4)
+        Me.Panel1.Controls.Add(Me.Button3)
+        Me.Panel1.Controls.Add(Me.CheckBox3)
+        Me.Panel1.Controls.Add(Me.CheckBox2)
+        Me.Panel1.Controls.Add(Me.CheckBox1)
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Location = New System.Drawing.Point(12, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(914, 538)
+        Me.Panel1.TabIndex = 3
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(19, 3)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Clear"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(533, 4)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(104, 17)
+        Me.CheckBox3.TabIndex = 5
+        Me.CheckBox3.Text = "Auto Corrections"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(410, 4)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(117, 17)
+        Me.CheckBox2.TabIndex = 4
+        Me.CheckBox2.Text = "Manual Corrections"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(343, 3)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(61, 17)
+        Me.CheckBox1.TabIndex = 3
+        Me.CheckBox1.Text = "Original"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog2
+        '
+        Me.OpenFileDialog2.FileName = "OpenFileDialog2"
+        '
+        'CheckBox4
+        '
+        Me.CheckBox4.AutoSize = True
+        Me.CheckBox4.Location = New System.Drawing.Point(804, 5)
+        Me.CheckBox4.Name = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(107, 17)
+        Me.CheckBox4.TabIndex = 7
+        Me.CheckBox4.Text = "Number Fixations"
+        Me.CheckBox4.UseVisualStyleBackColor = True
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(746, 3)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {1000, 0, 0, 0})
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {1000, 0, 0, -2147483648})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(54, 20)
+        Me.NumericUpDown1.TabIndex = 8
+        '
+        'NumericUpDown2
+        '
+        Me.NumericUpDown2.AllowDrop = True
+        Me.NumericUpDown2.Location = New System.Drawing.Point(663, 4)
+        Me.NumericUpDown2.Maximum = New Decimal(New Integer() {1000, 0, 0, 0})
+        Me.NumericUpDown2.Minimum = New Decimal(New Integer() {1000, 0, 0, -2147483648})
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        Me.NumericUpDown2.Size = New System.Drawing.Size(57, 20)
+        Me.NumericUpDown2.TabIndex = 9
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(643, 7)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(14, 13)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "X"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(726, 5)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(14, 13)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Y"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(100, 2)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 12
+        Me.Button4.Text = "Refresh"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(962, 594)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Eye Tracking Correction"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents OpenFileDialog2 As OpenFileDialog
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents NumericUpDown2 As NumericUpDown
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents Button4 As Button
+End Class
---
+++ vizmanip-application/WindowsApplication1/Form1.vb
@@ -0,0 +1,132 @@
+﻿Public Class Form1
+    Dim tableData As New Dictionary(Of Integer, String())
+    Dim rowCount As Integer = 0
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            PictureBox1.Load(OpenFileDialog1.FileName)
+        End If
+
+    End Sub
+
+    Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
+
+    End Sub
+
+    Private Sub OpenFileDialog2_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog2.FileOk
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If OpenFileDialog2.ShowDialog() = DialogResult.OK Then
+            Using MyReader As New Microsoft.VisualBasic.FileIO.TextFieldParser(OpenFileDialog2.FileName)
+                MyReader.TextFieldType = FileIO.FieldType.Delimited
+                MyReader.SetDelimiters(",")
+                Dim currentRow As String()
+                REM Dim tableData As New Dictionary(Of Integer, String())
+                REM Dim rowCount As Integer = 0
+                tableData.Clear()
+                rowCount = 0
+                While Not MyReader.EndOfData
+                    Try
+                        currentRow = MyReader.ReadFields()
+                        REM Dim currentField As String
+                        tableData.Add(rowCount, currentRow)
+                        rowCount += 1
+                        REM For Each currentField In currentRow
+                        REM MsgBox(currentField)
+                        REM Next
+                    Catch ex As Microsoft.VisualBasic.
+                                FileIO.MalformedLineException
+                        MsgBox("Line " & ex.Message &
+                        "is not valid and will be skipped.")
+                    End Try
+                End While
+                REM MsgBox(rowCount)
+                display_points(tableData, rowCount)
+            End Using
+        End If
+    End Sub
+    Private Sub redraw_points(sender As Object, e As EventArgs) Handles PictureBox1.Paint
+        display_points(tableData, rowCount)
+
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        PictureBox1.Refresh()
+    End Sub
+    Private Sub display_points(tableData As Dictionary(Of Integer, String()), rowCount As Integer)
+        Dim currentRow As String()
+        Dim nextRow As String()
+        Dim x As Integer
+        Dim y As Integer
+        Dim x_man_cor As Integer
+        Dim y_man_cor As Integer
+        Dim x_auto_cor As Integer
+        Dim y_auto_cor As Integer
+        Dim p As New System.Drawing.Pen(Color.Red, 4)
+        Dim p2 As New System.Drawing.Pen(Color.Blue, 1)
+        Dim p_man As New System.Drawing.Pen(Color.Blue, 4)
+        Dim p2_man As New System.Drawing.Pen(Color.Red, 1)
+        Dim p_auto As New System.Drawing.Pen(Color.Green, 4)
+        Dim p2_auto As New System.Drawing.Pen(Color.Orange, 1)
+        Dim g As System.Drawing.Graphics
+        Dim f As New System.Drawing.Font(FontFamily.GenericSansSerif, 18, FontStyle.Bold)
+        Dim mybrush As New SolidBrush(Color.Black)
+        g = PictureBox1.CreateGraphics
+
+        For counter As Integer = 0 To rowCount - 1
+            currentRow = tableData.Values(counter)
+            REM find header data
+            If counter = 0 Then
+                Dim fieldCounter As Integer = 0
+                For Each currentField In currentRow
+                    If currentField = "x" Then
+                        x = fieldCounter
+                    ElseIf currentField = "y" Then
+                        y = fieldCounter
+                    ElseIf currentField = "x-man-cor" Then
+                        x_man_cor = fieldCounter
+                    ElseIf currentField = "y-man-cor" Then
+                        y_man_cor = fieldCounter
+                    ElseIf currentField = "x-auto-cor" Then
+                        x_auto_cor = fieldCounter
+                    ElseIf currentField = "y-auto-cor" Then
+                        y_auto_cor = fieldCounter
+                    End If
+                    fieldCounter += 1
+                Next
+            ElseIf counter < rowCount - 1
+                nextRow = tableData.Values(counter + 1)
+                If CheckBox1.Checked = True Then
+                    g.DrawEllipse(p, CInt(currentRow(x) - 2), CInt(currentRow(y) - 2), 4, 4)
+                    g.DrawLine(p2, CSng(currentRow(x)), CSng(currentRow(y)), CSng(nextRow(x)), CSng(nextRow(y)))
+                    If CheckBox4.Checked = True Then
+                        g.DrawString(CStr(counter), f, mybrush, CSng(currentRow(x)), CSng(currentRow(y)))
+                    End If
+                End If
+                If CheckBox2.Checked = True Then
+                    g.DrawEllipse(p_man, CInt(currentRow(x_man_cor) - 2), CInt(currentRow(y_man_cor) - 2), 4, 4)
+                    g.DrawLine(p2_man, CSng(currentRow(x_man_cor)), CSng(currentRow(y_man_cor)), CSng(nextRow(x_man_cor)), CSng(nextRow(y_man_cor)))
+                    If CheckBox4.Checked = True Then
+                        g.DrawString(CStr(counter), f, mybrush, CSng(currentRow(x_man_cor)), CSng(currentRow(y_man_cor)))
+
+                    End If
+                End If
+                If CheckBox3.Checked = True Then
+                    g.DrawEllipse(p_auto, CInt(currentRow(x_auto_cor) - 2), CInt(currentRow(y_auto_cor) - 2), 4, 4)
+                    g.DrawLine(p2_auto, CSng(currentRow(x_auto_cor)), CSng(currentRow(y_auto_cor)), CSng(nextRow(x_auto_cor)), CSng(nextRow(y_auto_cor)))
+                    If CheckBox4.Checked = True Then
+                        g.DrawString(CStr(counter), f, mybrush, CSng(currentRow(x_auto_cor)), CSng(currentRow(y_auto_cor)))
+                    End If
+                End If
+            End If
+        Next
+        REM PictureBox1.Refresh()
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+
+    End Sub
+    Private Sub button4_click(sender As Object, e As EventArgs) Handles Button4.Click
+        display_points(tableData, rowCount)
+    End Sub
+End Class
